e88bc60c8f4b0a1d2d96c9cec33bde7d4e953092
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-04-28 23:57:05 +0100

    button workling correctly and adding items to the database

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://shopping-list-app-45848-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDb = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+    let inputValue = inputFieldEl.value;
+
+    push(shoppingListInDb, inputValue);
     
-    console.log(inputValue)
+    console.log(inputValue);
 })
